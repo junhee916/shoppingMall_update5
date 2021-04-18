de0cabc2b39f7dab5c87735755d1e432d6bb045f
junhee916 <junhee916@naver.com>
2021-04-18 11:05:57 +0900

    restApi started

---
+++ router/product.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.json({
+        message : "get product"
+    })
+})
+
+router.post("/", (req, res) => {
+
+    const newProduct = {
+        name : req.body.productName,
+        price : req.body.productPrice
+    }
+
+    res.json({
+        message : "register product",
+        productInfo : newProduct
+    })
+})
+
+router.patch("/", (req, res) => {
+    res.json({
+        message : "updated product"
+    })
+})
+
+router.delete("/", (req, res) => {
+    res.json({
+        message : "delete product"
+    })
+})
+
+module.exports = router
